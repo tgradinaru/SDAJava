800a4ab583a5f8e8901d6aaf9b2a7008f04dd766
tgradinaru <traian.gradinaru@gmail.com>
2021-01-23 14:29:34 +0200

    Task 1 Static vs non-static, final

---
+++ src/main/java/fundamentals/TasksExercises.java
@@ -0,0 +1,8 @@
+package fundamentals;
+
+public class TasksExercises {
+    // Write an application that will read diameter of a circle (variable of type float) and
+    //calculate perimeter of given circle.
+    //Firstly, assume π = 3.14. Later, use value of π from built-in Math class.
+
+}
