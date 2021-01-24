524eecd638d38aeb1573f6cb492de5c0b4bafa99
tgradinaru <traian.gradinaru@gmail.com>
2021-01-24 10:36:21 +0200

    Task 2: Body mass index

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -7,20 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         //static vs non-static fields and methods
-        TasksExercises tasksExercises1 = new TasksExercises();
-        TasksExercises tasksExercises2 = new TasksExercises();
-        System.out.println("x1 = " + tasksExercises1.x);
-        System.out.println("x2 = " + tasksExercises2.x);
-        System.out.println("y1 = " + tasksExercises1.y);
-        System.out.println("y2 = " + tasksExercises2.y);
-        tasksExercises1.x = 22;
-        tasksExercises1.y = 3;
-        System.out.println("x1 = " + tasksExercises1.x);
-        System.out.println("x2 = " + tasksExercises2.x);
-        System.out.println("y1 = " + tasksExercises1.y);
-        System.out.println("y2 = " + tasksExercises2.y);
+//        TasksExercises tasksExercises1 = new TasksExercises();
+//        TasksExercises tasksExercises2 = new TasksExercises();
+//        System.out.println("x1 = " + tasksExercises1.x);
+//        System.out.println("x2 = " + tasksExercises2.x);
+//        System.out.println("y1 = " + tasksExercises1.y);
+//        System.out.println("y2 = " + tasksExercises2.y);
+//        tasksExercises1.x = 22;
+//        TasksExercises.y = 3;
+//        System.out.println("x1 = " + tasksExercises1.x);
+//        System.out.println("x2 = " + tasksExercises2.x);
+//        System.out.println("y1 = " + tasksExercises1.y);
+//        System.out.println("y2 = " + tasksExercises2.y);
 
         //Task 1
-        tasksExercises1.circlePerimeter();
+        //tasksExercises1.circlePerimeter();
+        //Task 2
+        TasksExercises.bodyMassIndex();
     }
 }
--- src/main/java/fundamentals/TasksExercises.java
+++ src/main/java/fundamentals/TasksExercises.java
@@ -16,4 +16,27 @@ public class TasksExercises {
         float diameter = scanner.nextFloat();
         System.out.println("The perimeter has the folowing length: " + diameter * pi);
     }
+
+    //Write an application calculating BMI (Body Mass Index) and checking if itЀs optimal or not.
+    // Your application should read two variables: weight (in kilograms, type float)
+    // and height (in centimeters, type int). BMI should be calculated given
+    // following formula: weight/height(m)^2 The optimal BMI range is from 18.5 to 24.9,
+    // smaller or larger values are non-optimal values. Your program should write "BMI optimal"
+    // or "BMI not optimal", according to the assumptions above.
+    public static void bodyMassIndex() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert your height in cm, e.g. 187 : ");
+        int height = scanner.nextInt();
+        System.out.println("Insert your weight in Kg, e.g. 96.5: ");
+        float weight = scanner.nextFloat();
+        float bmi = weight / ((float) height * (float) height / 10000);
+
+        if ((bmi >= 18.5f) && (bmi <= 24.9)) {
+            System.out.println(bmi + " BMI optimal");
+        } else
+            System.out.println(bmi + " BMI not optimal");
+    }
+
+    //
+
 }
