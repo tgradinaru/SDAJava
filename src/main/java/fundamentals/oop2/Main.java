f0bb630934fafdc7951c2093f9f73b034e7e70cc
tgradinaru <traian.gradinaru@gmail.com>
2021-02-06 01:13:24 +0200

    oop2 Homework 2021.02.06 +classes: Product and Main

---
+++ src/main/java/fundamentals/oop2/Main.java
@@ -0,0 +1,20 @@
+package fundamentals.oop2;
+
+/**
+ * Implement all the classes with methods and fields necessary for the following scenario:
+ * A store has to sell multiple products:
+ * A product is defined by: name, price, expiration date.
+ * The price of the product can be change but only with a difference of max 14% of the original price.
+ * The store can inform the seller with the following informations if he asks for them:
+ * which is the list of products with price bigger than a specified price
+ * which is the list of products that have a the expiration date the next day
+ * the expiration date of the product with a certain name
+ * all the names of the products that have already expired
+ * The seller can also:
+ * modify the price of all the products from the store that have 30 days until the expiration date by
+ *                                                                          reducing the price with 15%
+ * rename  a specific product that has a certain name, from the list
+ */
+
+public class Main {
+}
